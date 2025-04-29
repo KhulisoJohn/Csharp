f923282d1de246d79cd17c25f1b57bddb8dcf5fc
khulysojohn <khuliso.thavhiwa@techbridlefoundation.org>
2025-04-29 19:04:26 +0200

    file modified

--- 1.C#-basics/StringInterpolation/Program.cs
+++ 1.C#-basics/StringInterpolation/Program.cs
@@ -25,6 +25,11 @@
             string projectName = "First-Project";
             Console.WriteLine($@"c:\Output\{projectName}\Data");
 
+            //Challenge Format and display instructions
+            string workName = "ACME";
+            string russianMessege = "\u043e\u0441\u043c\u043e\u0442\u0435\u044c\u0440\u0443\u0441\u043a\u0438\u0439\u0432\u043e\u0434";
+            Console.WriteLine($"{workName} {russianMessege}");
+
         }
     }
  }
