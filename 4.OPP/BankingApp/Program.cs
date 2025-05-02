1bee38ffc8685335c497a383eaf2077270357a17
khulysojohn <khuliso.thavhiwa@techbridlefoundation.org>
2025-05-02 03:41:10 +0200

    initial commit

---
+++ 4.OPP/BankingApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace BankingApp {
+    class BankAccount
+    {
+
+        private double balance;
+        public void Deposit(double amount)
+        {
+            balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount <= balance)
+            {
+                balance -= amount;
+            }
+            else {
+                Console.WriteLine("Inficient Funds");
+            }
+        }
+        public double GetBalance() {
+            return balance;
+        }
+    }
+
+    class Program {
+        static void Main()
+        {
+            BankAccount account = new BankAccount();
+            account.Deposit(1000);
+            account.Withdraw(500);
+            Console.WriteLine($"Balance: {account.GetBalance()}");
+            
+        }
+    }
+    
+}
