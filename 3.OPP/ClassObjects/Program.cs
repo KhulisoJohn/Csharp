2ad34c8829083d98ab9b61ea2999cc3dfc0f1675
khulysojohn <khuliso.thavhiwa@techbridlefoundation.org>
2025-05-02 02:46:55 +0200

    initial commit

---
+++ 3.OPP/ClassObjects/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Student
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public void DisplayDetails()
+    {
+        Console.WriteLine($"Name: {Name}, Age: {Age}");
+    }
+}
+
+class Teacher
+{
+    public string Name { get; set; }
+    public string Subject { get; set; }
+
+    public void DisplayDetails()
+    {
+        Console.WriteLine($"Teacher: {Name}, Subject: {Subject}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student s = new Student { Name = "John", Age = 33 };
+        s.DisplayDetails();
+
+        Teacher t = new Teacher { Name = "Mrs. Smith", Subject = "Math" };
+        t.DisplayDetails(); 
+    }
+}
