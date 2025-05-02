5fc8235eb712e53c7a1f10ff102e6f14cebfacd5
khulysojohn <khuliso.thavhiwa@techbridlefoundation.org>
2025-05-02 02:01:01 +0200

    new file added

---
+++ 2.Control-flow/ConditionLoops/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ControlLoops
+
+{
+    class Program
+    {
+        static void Main()
+        {
+            //conditional statement example
+            int number = 5;
+            if (number > 0) Console.WriteLine("Positive");
+            else if (number < 0) Console.WriteLine("Negative");
+            else Console.WriteLine("zero");
+
+            //Loop Example
+            int sum = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                sum += i;
+                Console.WriteLine(i);
+            }
+            Console.WriteLine($"The sum is equal to: {sum}");
+    }
+ }    
+}
