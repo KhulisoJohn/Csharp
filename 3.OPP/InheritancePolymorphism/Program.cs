557827710f91201541fe02025dcca844c29195cc
khulysojohn <khuliso.thavhiwa@techbridlefoundation.org>
2025-05-02 02:59:00 +0200

    new file added

---
+++ 3.OPP/InheritancePolymorphism/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace InheritancePolymorphism
+{
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some Generic animal sound");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Back");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meaw");
+        }
+    }
+
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Animal dog = new Dog();
+            Animal cat = new Cat();
+            dog.MakeSound();
+            cat.MakeSound();
+        }
+    }
+}
