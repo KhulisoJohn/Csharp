1aa01c3644768cfd6586cb4659de331e87beedb6
khulysojohn <khuliso.thavhiwa@techbridlefoundation.org>
2025-05-03 03:52:54 +0200

    new file added

---
+++ 2.Control-flow/OverloadedMethod/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OverloadedMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 7;
+            string text = "seven";
+            Console.WriteLine(number);
+            Console.WriteLine();
+            Console.WriteLine(text);
+        }
+    }
+}
