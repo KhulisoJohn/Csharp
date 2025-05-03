fa1264677d23bc92ffaeff20c2122698bb0dac0c
khulysojohn <khuliso.thavhiwa@techbridlefoundation.org>
2025-05-03 03:06:11 +0200

    new file added

---
+++ 2.Control-flow/ClassMethod/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ClassMethod
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Random dice = new Random();
+            int roll = dice.Next(1, 7);
+            Console.WriteLine(roll);
+        }
+    }
+}
