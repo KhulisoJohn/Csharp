841fc4d11515b05dcab5eb3e22138976f55dcb88
khulysojohn <khuliso.thavhiwa@techbridlefoundation.org>
2025-05-06 03:33:53 +0200

    new update

--- 1.C#-basics/StringConcatenation/obj/Debug/net7.0/StringConcatenation.AssemblyInfo.cs
+++ 1.C#-basics/StringConcatenation/obj/Debug/net7.0/StringConcatenation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StringConcatenation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4d70f49d18abab8a5e052d161132da6accb8128")]
 [assembly: System.Reflection.AssemblyProductAttribute("StringConcatenation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StringConcatenation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- 4.OPP/ClassObjects/obj/Debug/net7.0/ClassObjects.AssemblyInfo.cs
+++ 4.OPP/ClassObjects/obj/Debug/net7.0/ClassObjects.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassObjects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4d70f49d18abab8a5e052d161132da6accb8128")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassObjects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassObjects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
