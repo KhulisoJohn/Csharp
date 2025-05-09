4c52efd3662be8018f045aec5b4dd59e0289f1d9
khulysojohn <khuliso.thavhiwa@techbridlefoundation.org>
2025-05-09 10:44:31 +0200

    initial commit

---
+++ 2.Control-flow/ForEach/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ForEach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inventory = [200, 450, 700, 175, 250];
+            int sum = 0;
+            int bin = 0;
+
+            foreach (int items in inventory)
+            {
+                sum = sum + items;
+                bin++;
+                Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+            }
+
+            Console.WriteLine($"We have {sum} items in inventory.");
+            
+        }
+       
+    }
+}
