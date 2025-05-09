d757bff23b60ede32403d262a46d57339aaf34df
khulysojohn <khuliso.thavhiwa@techbridlefoundation.org>
2025-05-09 16:05:21 +0200

    new files added

---
+++ 2.Control-flow/NameConvesation/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace NameConversation {
+    class Program {
+        
+        static void Main(string[] args)
+        {
+             /* 
+        The following code creates five random OrderIDs to 
+        test the froud detection process. OrderIDs consist of a 
+        latter from A to E, and a three digit number. eg: A123
+        */
+            Random random = new Random();
+            string[] ordersIDs = new string[5];
+
+            for (int i = 0; i < ordersIDs.Length; i++)
+            {
+
+                int prefixValue = random.Next(65, 70);
+
+                string prefix = Convert.ToChar(prefixValue).ToString();
+
+                string suffix = random.Next(1, 1000).ToString("000");
+
+                ordersIDs[i] = prefix + suffix;
+            }
+
+            foreach (var orderID in ordersIDs)
+            {
+                Console.WriteLine(orderID);
+            }
+        }
+    }
+}
---
+++ 2.Control-flow/Whitespace/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Whitespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            This code reverses a messege, count the number of times a particula
+             character appear, then print the results to the console window.
+            */
+
+            string origionalMessege = "The quick brown fox jump over the lazy dog.";
+
+            char[] messege = origionalMessege.ToCharArray();
+            Array.Reverse(messege);
+
+            int letterCount = 0;
+
+            foreach (char letter in messege)
+            {
+                if (letter == '0')
+                {
+                    letterCount++;
+                }
+            }
+            string newMessege = new string(messege);
+
+            Console.WriteLine(newMessege);
+            Console.WriteLine($"'o' appears {letterCount} times");
+        }
+    }
+}
